49df1957feea3625e142a9efe95146d920141ad4
Ajay E S <ajay.s@jiffy.ai>
2023-01-06 19:43:05 +0530

    Make spring boot app to deploy in external tomcat

--- src/main/java/com/example/demo1/controller/RestController.java
+++ src/main/java/com/example/demo1/controller/RestController.java
@@ -3,13 +3,14 @@ package com.example.demo1.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @org.springframework.web.bind.annotation.RestController
-@RequestMapping("/mycontroller")
+@RequestMapping("/sample")
 public class RestController {
 
-    @GetMapping("/name")
-    public String getName(){
-        return "Ajay";
+    @GetMapping("/display/{name}")
+    public String getName(@PathVariable String name){
+        return "Hello "+name+ ", welcome to the application!";
     }
 }
